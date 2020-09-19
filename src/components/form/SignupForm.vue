c0a8639a73452355f54d845f9a1df92cef9118cb
hyeonic <evan3566@gmail.com>
2020-09-19 22:01:29 +0900

    feat: Add SignupForm.vue
    SignupForm 추가
    SignupPage, SignupForm css 스타일링
    SignupPage에 signup-form 태그 적용

---
+++ src/components/form/SignupForm.vue
@@ -0,0 +1,100 @@
+<template>
+  <form @submit.prevent="">
+    <div class="input-wrap">
+      <input
+        class="input-signup"
+        type="text"
+        v-model="username"
+        placeholder="아이디"
+      />
+    </div>
+    <div class="input-wrap">
+      <input
+        class="input-signup"
+        type="password"
+        v-model="password"
+        placeholder="비밀번호"
+      />
+    </div>
+    <div class="input-wrap">
+      <input
+        class="input-signup"
+        type="password"
+        v-model="passwordCheck"
+        placeholder="비밀번호 확인"
+      />
+    </div>
+    <div class="log-message">
+      {{ logMessage }}
+    </div>
+    <div class="button-wrap">
+      <button type="submit">Let's run with us!</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // form value
+      username: '',
+      password: '',
+      passwordCheck: '',
+
+      // Log
+      logMessage: '--',
+    };
+  },
+};
+</script>
+
+<style scoped>
+.input-wrap {
+  padding: 4%;
+  width: 90%;
+  border: 2px solid #bce0fd;
+  border-radius: 50px;
+  margin: 2rem auto 1rem auto;
+}
+.input-signup {
+  font-size: 1rem;
+  width: 95%;
+  color: #2699fb;
+}
+
+.input-signup::placeholder {
+  color: #bce0fd;
+}
+
+.input-signup:focus {
+  outline: none;
+}
+
+.log-message {
+  text-align: center;
+  padding: 10%;
+}
+
+.button-wrap {
+  width: 90%;
+  border: 2px solid #bce0fd;
+  border-radius: 50px;
+  margin: 2rem auto 1rem auto;
+}
+
+.button-wrap > button {
+  width: 100%;
+  height: 100%;
+  border: 0;
+  border-radius: 50px;
+  background-color: #2699fb;
+  padding: 4%;
+  font-size: 1rem;
+  color: white;
+}
+
+.button-wrap > button:focus {
+  outline: none;
+}
+</style>
--- src/views/SignupPage.vue
+++ src/views/SignupPage.vue
@@ -1,11 +1,57 @@
 <template>
-  <div>
-    signup page
+  <div id="sign-container">
+    <div class="sign-header">
+      <div class="logo">
+        <router-link to="/main">MajorRunner</router-link>
+      </div>
+    </div>
+    <div class="sign-section">
+      <signup-form></signup-form>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import SignupForm from '@/components/form/SignupForm.vue';
+
+export default {
+  components: {
+    SignupForm,
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+#sign-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+}
+
+.sign-header {
+  flex: 1;
+  background-color: #2699fb;
+  width: 100%;
+  height: 100%;
+}
+
+.logo {
+  padding-top: 20%;
+  text-align: center;
+}
+
+.logo > a {
+  font-size: 2rem;
+  color: white;
+  font-family: Berlin Sans FB;
+}
+
+.sign-section {
+  flex: 4;
+}
+</style>
