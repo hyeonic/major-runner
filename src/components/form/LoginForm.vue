4b004cf5886ba3cd346141ea09c3906530ab69e4
hyeonic <evan3566@gmail.com>
2020-09-19 21:45:27 +0900

    feat: Add LoginFrom.vue
    LoginForm 추가
    LoginPage, LoginForm css 스타일링
    LoginPage에 login-form 태그 적용

---
+++ src/components/form/LoginForm.vue
@@ -0,0 +1,91 @@
+<template>
+  <form @submit.prevent="">
+    <div class="input-wrap">
+      <input
+        class="input-login"
+        type="text"
+        v-model="username"
+        placeholder="아이디"
+      />
+    </div>
+    <div class="input-wrap">
+      <input
+        class="input-login"
+        type="password"
+        v-model="password"
+        placeholder="비밀번호"
+      />
+    </div>
+    <div class="log-message">
+      {{ logMessage }}
+    </div>
+    <div class="button-wrap">
+      <button type="submit">Run!</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // form value
+      username: '',
+      password: '',
+
+      // Log
+      logMessage: '--',
+    };
+  },
+};
+</script>
+
+<style scoped>
+.input-wrap {
+  padding: 4%;
+  width: 90%;
+  border: 2px solid #bce0fd;
+  border-radius: 50px;
+  margin: 2rem auto 1rem auto;
+}
+.input-login {
+  font-size: 1rem;
+  width: 95%;
+  color: #2699fb;
+}
+
+.input-login::placeholder {
+  color: #bce0fd;
+}
+
+.input-login:focus {
+  outline: none;
+}
+
+.log-message {
+  text-align: center;
+  padding: 10%;
+}
+
+.button-wrap {
+  width: 90%;
+  border: 2px solid #bce0fd;
+  border-radius: 50px;
+  margin: 2rem auto 1rem auto;
+}
+
+.button-wrap > button {
+  width: 100%;
+  height: 100%;
+  border: 0;
+  border-radius: 50px;
+  background-color: #2699fb;
+  padding: 4%;
+  font-size: 1rem;
+  color: white;
+}
+
+.button-wrap > button:focus {
+  outline: none;
+}
+</style>
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,11 +1,57 @@
 <template>
-  <div>
-    login page
+  <div id="login-container">
+    <div class="login-header">
+      <div class="logo">
+        <router-link to="/main">MajorRunner</router-link>
+      </div>
+    </div>
+    <div class="login-section">
+      <login-form></login-form>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import LoginForm from '@/components/form/LoginForm.vue';
+
+export default {
+  components: {
+    LoginForm,
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+#login-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+}
+
+.login-header {
+  flex: 1;
+  background-color: #2699fb;
+  width: 100%;
+  height: 100%;
+}
+
+.logo {
+  padding-top: 20%;
+  text-align: center;
+}
+
+.logo > a {
+  font-size: 2rem;
+  color: white;
+  font-family: Berlin Sans FB;
+}
+
+.login-section {
+  flex: 4;
+}
+</style>
